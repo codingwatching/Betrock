bd4aa04a82dfe839406dc34cbb889d534f1b6c2a
TVirtmannVisy <23031327+OfficialPixelBrush@users.noreply.github.com>
2026-05-06 10:41:35 +0200

    This is awful, but it works

--- src/world/loaders/worldLoader.cpp
+++ src/world/loaders/worldLoader.cpp
@@ -1,4 +1,6 @@
 #include "worldLoader.h"
+#include "nbt.h"
+#include <cstddef>
 WorldLoader::WorldLoader(std::string pPath) {
 	WorldLoader::path = pPath;
 }
@@ -10,73 +12,57 @@ Chunk* WorldLoader::extractNbtChunk(int chunkX, int chunkZ, uint8_t* nbtData, si
     std::istringstream stream(s);
 	// Extract Block Data
 	//if (lastX != chunkX && lastZ != chunkZ) {
-    auto readRoot = NbtRead(stream);
+    auto baseRoot = std::dynamic_pointer_cast<CompoundNbtTag>(NbtRead(stream, NBT_UNCOMPRESSED));
+    auto root = std::dynamic_pointer_cast<CompoundNbtTag>(baseRoot->Get("Level"));
 	//}
-	free(nbtData);
-	if (!readRoot) {
+	if (!root) {
 		std::cerr << "The entry is not of type TAG_Compound!" << std::endl;
 		//continue;
 		return nullptr;
 	}
-    std::cout << *readRoot;
 
-    /*
 	// Get Block Data
-	int8_t* blockData;
-	int8_t* blockSkyLightData;
-	int8_t* blockLightData;
-	int8_t* blockMetaData;
-	bool foundBlockData = false;
-	bool foundSkyLightData = false;
-	bool foundLightData = false;
-	bool foundMetaData = false;
-	for (uint i = 0; i < chunkLevel->getSizeOfData(); i++) {
-		// Get Block ID
-		if (chunkLevel->getData(i)->getName() == "Blocks") {
-			auto* blockArray = dynamic_cast<TAG_Byte_Array*>( chunkLevel->getData(i) );
-			blockData = blockArray->getData();
-			foundBlockData = true;
-		}
-		// Get Block Sky Light
-		if (chunkLevel->getData(i)->getName() == "SkyLight") {
-			auto* blockSkyLightArray = dynamic_cast<TAG_Byte_Array*>( chunkLevel->getData(i) );
-			blockSkyLightData = blockSkyLightArray->getData();
-			foundSkyLightData = true;		
-		}
-		// Get Block Light
-		if (chunkLevel->getData(i)->getName() == "BlockLight") {
-			auto* blockLightArray = dynamic_cast<TAG_Byte_Array*>( chunkLevel->getData(i) );
-			blockLightData = blockLightArray->getData();
-			foundLightData = true;		
-		}
-		// Get Block Metadata
-		if (chunkLevel->getData(i)->getName() == "Data") {
-			auto* blockMetaDataArray = dynamic_cast<TAG_Byte_Array*>( chunkLevel->getData(i) );
-			blockMetaData = blockMetaDataArray->getData();
-			foundMetaData = true;
-		}
-		// If all has been found, gtfo
-		if (foundBlockData && foundSkyLightData && foundLightData && foundMetaData) {
-			break;
-		}
-	}
-	if (!blockData) {
+	uint8_t* blockData;
+	uint8_t* blockSkyLightData;
+	uint8_t* blockLightData;
+	uint8_t* blockMetaData;
+    auto blocks = std::dynamic_pointer_cast<ByteArrayNbtTag>(root->Get("Blocks"));
+    // Get Block ID
+    if (!blocks) {
 		std::cerr << "No block data found!" << std::endl;
-		return nullptr;
-	}
-	if (!blockSkyLightData) {
+        return nullptr;
+    }
+    const auto& blockVec = blocks->GetData();
+    blockData = new uint8_t[blockVec.size()];
+    memcpy(blockData, blockVec.data(), blockVec.size());
+    // Get Block Sky Light
+    auto skyLight = std::dynamic_pointer_cast<ByteArrayNbtTag>(root->Get("SkyLight"));
+    if (!skyLight) {
 		std::cerr << "No sky light data found!" << std::endl;
-		return nullptr;
-	}
-	if (!blockLightData) {
+        return nullptr;
+    }
+    const auto& skyVec = skyLight->GetData();
+    blockSkyLightData = new uint8_t[skyVec.size()];
+    memcpy(blockSkyLightData, skyVec.data(), skyVec.size());
+    // Get Block Light
+    auto blockLight = std::dynamic_pointer_cast<ByteArrayNbtTag>(root->Get("BlockLight"));
+    if (!blockLight) {
 		std::cerr << "No block light data found!" << std::endl;
-		return nullptr;
-	}
-	if (!blockMetaData) {
+        return nullptr;
+    }
+    const auto& lightVec = blockLight->GetData();
+    blockLightData = new uint8_t[lightVec.size()];
+    memcpy(blockLightData, lightVec.data(), lightVec.size());
+    // Get Block Metadata
+    auto meta = std::dynamic_pointer_cast<ByteArrayNbtTag>(root->Get("Data"));
+    if (!meta) {
 		std::cerr << "No block metadata found!" << std::endl;
-		return nullptr;
-	}
+        return nullptr;
+    }
+    const auto& metaVec = meta->GetData();
+    blockMetaData = new uint8_t[metaVec.size()];
+    memcpy(blockMetaData, metaVec.data(), metaVec.size());
+    // If all has been found, gtfo
+	free(nbtData);
 	return new Chunk(chunkX,chunkZ,blockData,blockSkyLightData,blockLightData,blockMetaData);
-        */
-    return nullptr;
 }
